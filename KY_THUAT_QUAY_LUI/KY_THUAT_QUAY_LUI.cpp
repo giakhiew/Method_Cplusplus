e3e77d6023e67cbfe1d2510840f7332c84130998
NMTThinh <khiemdo2009@gmail.com>
2025-10-18 16:24:22 +0700

    a

--- KY_THUAT_QUAY_LUI/KY_THUAT_QUAY_LUI.cpp
+++ KY_THUAT_QUAY_LUI/KY_THUAT_QUAY_LUI.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-int arr[101], used[101];
+int arr[101], arrh[101], arrw[101], used[101], solution[101];
+bool dcx[101], dcn[101], columnarr[101], rowarr[101];
 int n = 3;
 int k;
 
@@ -53,8 +54,65 @@ void inHoanVi(int pos) {
         }
     }
 }
-int main()
-{
-    inHoanVi(1);
+//dg cheo nguoc = i - j + n, dg cheo xuoi = i + j - 1; cot = arr
+void printRowColumn() {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (solution[i] == j) cout << 1 << " ";
+            else cout << 0 << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void NQuanHau(int row) {
+    for (int col = 1; col <= n; col++) {
+        int d2 = row - col + n, d1 = row + col - 1;
+
+        if (dcx[d1] == false && dcn[d2] == false && columnarr[col] == false) {
+
+            dcx[d1] = true;
+            dcn[d2] = true;
+            columnarr[col] = true;
+            solution[row] = col;
+
+            if (row < n) NQuanHau(row + 1);
+            else printRowColumn();
+            
+
+            dcx[d1] = false;
+            dcn[d2] = false;
+            columnarr[col] = false;
+        }
+    }
+}
+void initArrH() {
+    for (int i = 1; i <= n; i++) {
+        cin >> arrh[i];
+    }
+}
+
+//void initArrW(int n) {
+//    for (int i = 1; i < n; i++) {
+//        for (int j = i; j < n - i; j++) {
+//            cin >> arrw[j];
+//        }
+//    }
+//}
+
+int ans = 0, sum1 = 0, sum2 = 0;
+
+void doiDau(int pos) {
+    if (pos == n) {
+        ans = min(sum1, sum2);
+    }
+    
+}
+
+int main() {
+    initArrH();
+    doiDau(1);
+    cout << ans;
     return 0;
 }
